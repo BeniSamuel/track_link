fe7e71547dea84690417ed5bb2e019635743d923
benisamuel <benisamuel566@gmail.com>
2025-05-03 19:26:32 +0200

    Added vehicle repository

--- src/main/java/com/tracklink/www/repository/VehicleRepository.java
+++ src/main/java/com/tracklink/www/repository/VehicleRepository.java
@@ -1,4 +1,10 @@
 package com.tracklink.www.repository;
 
-public class VehicleRepository {
+import com.tracklink.www.model.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+    Optional<Vehicle> getVehicleById (Long id);
 }
