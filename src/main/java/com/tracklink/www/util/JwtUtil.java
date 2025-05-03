03a624b916a7ae919c7ce12e2393a1499a46effb
benisamuel <benisamuel566@gmail.com>
2025-05-03 19:00:09 +0200

    Added Jwtutil

--- src/main/java/com/tracklink/www/util/JwtUtil.java
+++ src/main/java/com/tracklink/www/util/JwtUtil.java
@@ -1,4 +1,51 @@
 package com.tracklink.www.util;
 
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
 public class JwtUtil {
+
+    @Value("${jwt.secret.key}")
+    private String SECRET_KEY;
+
+    public String generateToken (String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
+                .compact();
+    }
+
+    public String extractToken (String token) {
+        return Jwts.parser()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validateToken (String token, UserDetails userDetails) {
+        try {
+            String email = extractToken(token);
+            return email.equals(userDetails.getUsername()) && !isTokenExpired(token);
+        }
+        catch (JwtException e) {
+            return false;
+        }
+    }
+
+    public boolean isTokenExpired (String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody()
+                .getExpiration()
+                .before(new Date());
+    }
 }
