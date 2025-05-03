1e6d5b03a77d53cd6144eec729b6943bf3a81b69
benisamuel <benisamuel566@gmail.com>
2025-05-03 14:59:20 +0200

    Added JwtAuthFilter

--- src/main/java/com/tracklink/www/security/JwtAuthFilter.java
+++ src/main/java/com/tracklink/www/security/JwtAuthFilter.java
@@ -16,5 +16,10 @@ public class JwtAuthFilter extends OncePerRequestFilter {
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain) throws ServletException, IOException {
 
+            String authHeader = request.getHeader("Authorization");
+            if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                String token = authHeader.substring(7);
+                filterChain.doFilter(request,response);
+            }
     }
 }
