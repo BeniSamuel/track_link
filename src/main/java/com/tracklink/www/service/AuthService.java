4096bb16fffc41ff900a0ead990485f09786fdff
benisamuel <benisamuel566@gmail.com>
2025-05-03 18:41:36 +0200

    Added jsonwebtoken

--- src/main/java/com/tracklink/www/service/AuthService.java
+++ src/main/java/com/tracklink/www/service/AuthService.java
@@ -1,7 +1,16 @@
 package com.tracklink.www.service;
 
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
+    private final AuthenticationManager authenticationManager;
+    private final PasswordEncoder passwordEncoder;
+    private final
+
+    public AuthService () {
+
+    }
 }
