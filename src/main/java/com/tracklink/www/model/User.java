dc5967a5834fe02e1652c42260aa31130e244e27
benisamuel <benisamuel566@gmail.com>
2025-05-03 20:10:33 +0200

    Improved User Model

--- src/main/java/com/tracklink/www/model/User.java
+++ src/main/java/com/tracklink/www/model/User.java
@@ -10,7 +10,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
-    private Long userid;
+    private Long id;
 
     private String name;
 
@@ -34,14 +34,12 @@ public class User {
     }
 
     // Getters and Setters
-    public Long getUser_id () { return this.userid; }
     public String getName () { return this.name; }
     public String getEmail () { return this.email; }
     public String getPassword () { return this.password; }
     public int getPhone () { return this.phone; }
     public Role getRole () { return this.role; }
 
-    public void setUser_id (Long id) { this.userid = id; }
     public void setName (String name) { this.name = name; }
     public void setEmail (String email) { this.email = email; }
     public void setPassword (String password) { this.password = password; }
