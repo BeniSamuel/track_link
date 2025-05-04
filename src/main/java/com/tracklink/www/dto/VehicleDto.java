4678176eaceb7ec20001b64d570601d1e8692a0d
benisamuel <benisamuel566@gmail.com>
2025-05-04 22:02:47 +0200

    Added getters in vehicledto

--- src/main/java/com/tracklink/www/dto/VehicleDto.java
+++ src/main/java/com/tracklink/www/dto/VehicleDto.java
@@ -1,4 +1,11 @@
 package com.tracklink.www.dto;
 
 public class VehicleDto {
+    private Long longitude;
+    private Long latitude;
+    private Long price;
+
+    public Long getLongitude () { return this.longitude; }
+    public Long getLatitude () { return this.latitude; }
+    public Long getPrice () { return this.price; }
 }
