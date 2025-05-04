2a455a95e76fa661f666b0cdcbd0f6d84b5b89dd
benisamuel <benisamuel566@gmail.com>
2025-05-04 16:53:54 +0200

    Added Token Checking login and principal setting

--- src/main/java/com/tracklink/www/security/JwtAuthFilter.java
+++ src/main/java/com/tracklink/www/security/JwtAuthFilter.java
@@ -1,10 +1,15 @@
 package com.tracklink.www.security;
 
+import com.tracklink.www.util.JwtUtil;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -12,6 +17,14 @@ import java.io.IOException;
 
 @Component
 public class JwtAuthFilter extends OncePerRequestFilter {
+    private final JwtUtil jwtUtil;
+    private final CustomUserDetailsService userDetailsService;
+
+    public JwtAuthFilter (JwtUtil jwtUtil, CustomUserDetailsService userDetailsService) {
+        this.jwtUtil = jwtUtil;
+        this.userDetailsService = userDetailsService;
+    }
+
     @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
@@ -21,7 +34,20 @@ public class JwtAuthFilter extends OncePerRequestFilter {
             String authHeader = request.getHeader("Authorization");
             if (authHeader != null && authHeader.startsWith("Bearer ")) {
                 String token = authHeader.substring(7);
-                filterChain.doFilter(request,response);
+                try {
+                    String email = this.jwtUtil.extractToken(token);
+                    if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                        UserDetails userDetails = this.userDetailsService.loadUserByUsername(email);
+                        if (this.jwtUtil.validateToken(token, userDetails)) {
+                            Authentication userAuth = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                            SecurityContextHolder.getContext().setAuthentication(userAuth);
+                        }
+                    }
+                } catch (Exception e) {
+                    System.out.println("Exception related to AuthFilter");
+                    System.out.println(e.getMessage());
+                }
             }
+            filterChain.doFilter(request, response);
     }
 }
